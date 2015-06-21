fb3645b634b670d9e8db500e0874b2691bc44fd3
Jared Burrows <jaredsburrows@gmail.com>
2015-06-21 12:40:20 -0400

    Prepare scripts for descriptions

--- disable-root.sh
+++ disable-root.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat "disable-list.txt" | while read d; do tmp=$(echo $d | tr -d '\n' | awk {'print $1'}); adb shell "su -c 'pm disable $tmp'"; done
+cat "disable-list.txt" | while read d; do tmp=$(echo $d | awk {'print $1'}); adb shell "su -c 'pm disable $tmp'"; done
 
